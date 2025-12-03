e3ef7baa50f3b8c794e33e4ed26ee43dd15c8355
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-12-03 12:50:54 -0300

    feat(config): implement Google Cloud client for managing compute instances and refactor settings

--- pool-manager/pool_manager/utils/settings.py
+++ pool-manager/pool_manager/utils/settings.py
@@ -4,8 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class BasicSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    HASH_CHALLENGE: str = "0000"
-
     RABBITMQ_HOST: str = "localhost"
     RABBITMQ_PORT: int = 5672
     RABBITMQ_USER: str = "admin"
@@ -22,7 +20,32 @@ class BasicSettings(BaseSettings):
     CONNECTION_MAX_RETRIES: int = 10
     CONNECTION_RETRY_DELAY: int = 5
 
-    BLOCK_CREATION_INTERVAL: int = 60
+    GCP_PROJECT_ID: str = "your-gcp-project-id"
+    GCP_ZONE: str = "your-gcp-zone"
+    GCP_CREDENTIALS_PATH: str = "your-gcp-credentials-path"
+    GCP_INSTANCE_NAME_PREFIX: str = "miner-cpu"
+    GCP_MACHINE_TYPE: str = "e2-highcpu-4"
+    # Created with packer https://github.com/matiagimenez/pow-blockchain/tree/main/terraform/packer
+    GCP_SOURCE_IMAGE: str = "pow-miner-1718748034"
+
+    @property
+    def GCP_MACHINE_FULL_TYPE(self) -> str:
+        return f"zones/{self.GCP_ZONE}/machineTypes/{self.GCP_MACHINE_TYPE}"
+
+    @property
+    def GCP_VM_SOURCE_IMAGE(self) -> str:
+        return f"projects/{self.GCP_PROJECT_ID}/global/images/{self.GCP_SOURCE_IMAGE}"
+
+    @property
+    def GCP_SUBNETWORK(self) -> str:
+        return f"projects/{self.GCP_PROJECT_ID}/regions/{self.GCP_ZONE.rsplit('-', 1)[0]}/subnetworks/default"
+
+    @property
+    def CPU_MINER_STARTUP_SCRIPT(self) -> str:
+        return (
+            "#!/bin/bash\n"
+            "sudo docker run -d -p 5000:5000 --name cpu-pow-miner pow-miner:latest"
+        )
 
 
 Settings = BasicSettings()
